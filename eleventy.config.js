8348ac123ba95ca234754e3dd9c8675b3d4bae66
Stephen Grice <stephengrice@live.com>
2025-04-11 13:54:48 -0400

    Add svg logos and get working

--- eleventy.config.js
+++ eleventy.config.js
@@ -101,6 +101,7 @@ export default async function(eleventyConfig) {
 		// by default we use Eleventy’s built-in `slugify` filter:
 		// slugify: eleventyConfig.getFilter("slugify"),
 		// selector: "h1,h2,h3,h4,h5,h6", // default
+		// checkDuplicates: "false",
 	});
 	// LaTeX
 	eleventyConfig.addPlugin(mathjaxPlugin);
