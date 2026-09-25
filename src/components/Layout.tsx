69f2ada94fcece4b6280ecbbc34c1648ed7abb2a
Steve Grice <steve@pagekey.io>
2026-09-25 17:17:26 -0400

    fix: hide consulting, portfolio on mobile too

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -79,12 +79,12 @@ export default function Layout({ children, title, description }: { children: Rea
                             <Link to="/products" className="transition-colors hover:text-orange-500" onClick={() => setIsMobileMenuOpen(false)}>
                                 Products
                             </Link>
-                            <Link to="/consulting" className="transition-colors hover:text-orange-500" onClick={() => setIsMobileMenuOpen(false)}>
+                            {/* <Link to="/consulting" className="transition-colors hover:text-orange-500" onClick={() => setIsMobileMenuOpen(false)}>
                                 Consulting
                             </Link>
                             <Link to="/portfolio" className="transition-colors hover:text-orange-500" onClick={() => setIsMobileMenuOpen(false)}>
                                 Portfolio
-                            </Link>
+                            </Link> */}
                             <a href="/blog" className="transition-colors hover:text-orange-500" onClick={() => setIsMobileMenuOpen(false)}>
                                 Blog
                             </a>
